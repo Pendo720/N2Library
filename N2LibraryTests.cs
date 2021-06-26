a277fa405da0aea9c9cb7c7d0201675943239c9b
Jada-S <pendo720@gmail.com>
2021-06-26 20:53:47 +0100

    initial commit

---
+++ N2LibraryTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace N2Library
+{
+    public class N2LibraryTests
+    {
+        [Fact]
+        void allIsGood()
+        {
+            Assert.True(0 == 0);
+        }
+    }
+}
---
+++ Neuron.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace N2Library
+{
+    public class Neuron
+    {
+    }
+}
