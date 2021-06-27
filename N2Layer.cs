582e3fcc6fdc33211582704c08992688568ac977
Jada-S <pendo720@gmail.com>
2021-06-27 16:34:20 +0100

    implementation neuron

--- N2Layer.cs
+++ N2Layer.cs
@@ -4,6 +4,7 @@ namespace N2Library
 {
     public class N2Layer
     {
+        public static readonly int WEIGHTS_START_INDEX = 5;
         public int LayerId { get; set; }
         private List<Neuron> mNetNeurons;
 
@@ -15,25 +16,24 @@ namespace N2Library
 
         public N2Layer load(List<double> contents)
         {
-            /*Neuron neuron = new Neuron(LayerId, contents.ToArray()get(1).intValue());
-            neuron.setActivationValue(contents.get(2));
-            neuron.setGradient(contents.get(3));
-            int connections = contents.get(4).intValue();
+            Neuron neuron = new Neuron(LayerId, (int)contents.ToArray()[1]);
+            neuron.Activation = contents.ToArray()[2];
+            neuron.Gradient = contents.ToArray()[3];
+            int connections = (int)contents.ToArray()[4];
             for (int i = 0; i < connections * 2;)
             {
-                N2Weight con = new N2Weight(contents.get(WEIGHTS_ARE_FROM + i), contents.get(WEIGHTS_ARE_FROM + 1 + i));
-                neuron.getWeights().add(con);
+                N2Weight con = new N2Weight(contents.ToArray()[WEIGHTS_START_INDEX + i], contents.ToArray()[WEIGHTS_START_INDEX + 1 + i]);
+                neuron.Weights.Add(con);
                 i += 2;
             }
             mNetNeurons.Add(neuron);
-*/
             return this;
         }
 
-        public Neuron get(int index) => mNetNeurons.ToArray()[index];
+        public Neuron At(int index) => mNetNeurons.ToArray()[index];
 
-        public void add(Neuron n) => mNetNeurons.Add(n);
+        public void Add(Neuron n) => mNetNeurons.Add(n);
 
-        public int size() => mNetNeurons.Count;
+        public int Size() => mNetNeurons.Count;
     }
 }
